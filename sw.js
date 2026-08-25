28590af625d49b1f5617b2c12ee60a8188a6c8ec
danilobgmachado-eng <danilobg.machado@gmail.com>
2026-08-25 13:23:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker do Gerador de Propostas
 // Guarda o app inteiro no aparelho para abrir sem internet.
-const CACHE = 'propostas-dm-6cd90499';
+const CACHE = 'propostas-dm-5f0ee0d4';
 const ARQUIVOS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
